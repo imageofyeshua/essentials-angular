5a16b4abf57ff037f9def8295e2ff627e2674b44
Daniel Park <intheimageofyeshua@gmail.com>
2025-11-07 08:59:30 +0900

    Tasks Component to Display Name

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -10,8 +10,7 @@ export class User {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
