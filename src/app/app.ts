215020bfe0d61e48e1e9ebc3b5c61dfd2f3eca1f
Daniel Park <intheimageofyeshua@gmail.com>
2025-11-06 14:52:30 +0900

    Component Input

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from './user/user';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('essentials-angular');
+  users = DUMMY_USERS;
 }
